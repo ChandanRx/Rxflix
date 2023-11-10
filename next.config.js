656e3e961f9fe564f7d889cce0bb90c5cbe81624
Chandan rxn <107489996+ChandanRx@users.noreply.github.com>
2023-11-10 19:20:57 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    serverComponentsExternalPackages : 'mangoose',
-   
+    
     images:{
          domains:['occ-0-3933-116.1.nflxso.net']
     }
